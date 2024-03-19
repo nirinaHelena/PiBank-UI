a8b5607f984b77cd4b0a5390d5143f8e7d2a5e9c
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-19 11:14:51 +0300

    feat: change navbar header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 import  Header  from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <Header/>
         {children}
-
+        
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,104 +1,82 @@
-import React from "react";
-import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+"use client"
 
-// Replace "PiBankLogo.png" with the actual path to your image
+import React, { useState } from "react";
+import Link from "next/link";
+import useRouter from "next/router";
 import piBankLogo from "../../public/images/image_2024-03-18_113053753-removebg-preview.png";
 import Image from "next/image";
+import { users } from "./mock/UsersMock";
+import styles from "../styles/Navbar.module.css"
+interface linkProps {
+  name: string;
+  href: string;
+}
 
-export default function App() {
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Historiques",
-    "Balance",
-    "Transfer",
-    "Transactions",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
+const links: linkProps[] = [
+  { name: "Home", href: "/home" },
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Balance", href: "/balance" },
+  { name: "Transaction", href: "/transaction" },
+  { name: "Account", href: "/account" },
+];
 
+export default function Header  () {
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const router = useRouter();
+  const pathname = router.pathname;
   return (
-    <Navbar disableAnimation isBordered>
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle />
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
-        <Image
-            src = {piBankLogo}
+    <div className="w-full max-w-7xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-0 flex">
+      <div className="flex items-center">
+        <Link href="/home" className="w-32">
+          <Image
+            src={piBankLogo}
             alt="PiBank Logo"
             width="36"
             height="36"
             layout="fixed"
             objectFit="cover"
           />
-        <p className="font-bold text-inherit">PiBank</p>
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-        <Image
-            src = {piBankLogo}
-            alt="PiBank Logo"
-            width="36"
-            height="36"
-            layout="fixed"
-            objectFit="cover"
-          />    
-          <p className="font-bold text-inherit">PiBank</p>
-        </NavbarBrand>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Dashboard
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page" color="warning">
-            Balance
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Transactions
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2
-                  ? "warning"
-                  : index === menuItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
+          <h1 className="font-bold text-xl">PiBank</h1>
+        </Link>
+        <ul className="lg:flex gap-x-4 ml-14 hidden ">
+          {links.map((link, idx) => (
+            <div key={idx}>
+              {pathname === link.href ? (
+                <li>
+                  <Link
+                    href={link.href}
+                    className="font-semibold underline text-sm-5 "
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ) : (
+                <li>
+                  <Link className="font-normal text-sm-5" href={link.href}>
+                    {link.name}
+                  </Link>
+                </li>
+              )}
+            </div>
+          ))}
+        </ul>
+      </div>
+      <div className="flex items-center gap-x-8">
+        {users.map((user, index) => (
+          <li key={index} className={styles.list}>
+            <Link href="/profile">
+            <div className={styles.profileImage}>
+                {profileImage ? (
+                  <Image src={profileImage} alt="" width={100} height={100} />
+                ) : (
+                  <Image src={user.profile_picture} alt="" width={100} height={100} />
+                )}
+              </div>
             </Link>
-          </NavbarMenuItem>
+          </li>
         ))}
-      </NavbarMenu>
-    </Navbar>
+      </div>
+    </div>
   );
 }
+
