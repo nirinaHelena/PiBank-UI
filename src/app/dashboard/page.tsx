b3afa52b740641b633b8f7f23a4a8c70654b4d76
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-22 09:44:12 +0300

    feat: create chart

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,14 +3,14 @@ import MyCard from "./components/MyCard"
 import ExpensesStatistics from "./components/ExpensesStatistics"
 import Card from "./components/Card"
 import TransactionsList from "./components/TransactionsList"
+import RecentSales from "./components/RecentSales"
 
 
 const Dashboard= () => {
   return (
     <main className=" flex flex-col ">
     <div className="mx-auto max-w-5xl gap-6 justify-center flex flex-grow">
-      <MyCard />
-      <ExpensesStatistics/>
+      <RecentSales/>
       <Card title="Transactions récentes" content={<TransactionsList />} />
 
     </div>
