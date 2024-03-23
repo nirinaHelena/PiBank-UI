72e058c85bf43a51c9d21f3997d90fc908097218
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-23 17:52:50 +0300

    feat: install tailwind debug screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {cn} from "@/lib/utils"
 
 import  Header  from "@/components/header";
 
@@ -18,10 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className)}>
       <Header/>
         {children}
-        
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,9 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-debug-screens"),
+    require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
