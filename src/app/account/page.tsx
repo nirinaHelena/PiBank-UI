0fa295863824963dd26fc98f91eef0311786cd50
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-26 10:51:42 +0300

    feat: add button for dialog

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -12,6 +12,17 @@ import { ColumnDef } from "@tanstack/react-table";
 import React from "react";
 import  PageTitle  from "@/components/PageTitle";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog"
 
 type Props = {};
 type UserDetails = {
@@ -80,7 +91,36 @@ export default function AccountsPage({}: Props) {
     <div>
        <div className="flex flex-row justify-between pb-5 gap-5 w-full items-center">
        <PageTitle title="Users"/>
-       <Button className="rounded-md font-semibold ">Create account</Button>
+       <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="font-semibold  rounded-sm">Create account</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
       </div>
       <div>
       <DataTable columns={columns} data={data} />
