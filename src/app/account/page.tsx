2e63678fc3823acc0832df81595048988d529cfe
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-03-26 13:59:54 +0300

    feat: create form for create acoount

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -12,8 +12,6 @@ import { ColumnDef } from "@tanstack/react-table";
 import React from "react";
 import  PageTitle  from "@/components/PageTitle";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Dialog,
   DialogContent,
@@ -23,6 +21,7 @@ import {
   DialogTrigger,
   DialogFooter,
 } from "@/components/ui/dialog"
+import FormAccount from "@/components/Form"
 
 type Props = {};
 type UserDetails = {
@@ -97,28 +96,14 @@ export default function AccountsPage({}: Props) {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Create an Account</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Create a profile here. Click create when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        
+          <FormAccount/>
+
       </DialogContent>
     </Dialog>
       </div>
